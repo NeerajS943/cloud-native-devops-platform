3bfbe289b98dda467eb22637f0e88ea6a3058077
NeerajS <neerajchinnu9@gmail.com>
2026-04-27 13:56:00 +0530

    Implement task API with Redis queue and worker service

---
+++ services/worker/worker.py
@@ -0,0 +1,35 @@
+import redis
+import json
+import time
+
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    decode_responses=True
+)
+
+print("Worker started...")
+
+while True:
+
+    job = r.brpop("task_queue", timeout=5)
+
+    if job:
+
+        task_id = job[1]
+
+        print(f"Processing task {task_id}")
+
+        time.sleep(5)
+
+        task = {
+            "task_id": task_id,
+            "status":"completed"
+        }
+
+        r.set(
+            task_id,
+            json.dumps(task)
+        )
+
+        print(f"Completed {task_id}")
