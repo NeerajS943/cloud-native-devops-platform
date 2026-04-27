5dbecbd0194302d0c09ccde3424713ced07ab289
NeerajS <neerajchinnu9@gmail.com>
2026-04-27 13:28:59 +0530

    Add FastAPI task service scaffold

---
+++ services/api/app/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from uuid import uuid4
+import redis
+import json
+
+app = FastAPI(title="Cloud Native Task Platform")
+
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    decode_responses=True
+)
+
+@app.get("/")
+def health():
+    return {
+        "status":"running"
+    }
+
+@app.post("/tasks")
+def create_task():
+
+    task_id=str(uuid4())
+
+    task={
+        "task_id":task_id,
+        "status":"queued"
+    }
+
+    r.set(task_id, json.dumps(task))
+    r.lpush("task_queue", task_id)
+
+    return task
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id:str):
+
+    task=r.get(task_id)
+
+    if not task:
+        return {"error":"not found"}
+
+    return json.loads(task)
